92cefe9354d07dd1a4e046ba8d35af8d5e68e5ea
jvanhook93 <106854412+jvanhook93@users.noreply.github.com>
2026-01-16 11:10:27 -0600

    Change title Headings

--- audits/admin.py
+++ audits/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import AuditEvent
 
 admin.site.register(AuditEvent)
+
+admin.site.site_header = "IntegraNet Training Administration"
+admin.site.site_title = "IntegraNet Training"
+admin.site.index_title = "Training Portal Dashboard"
--- config/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "IntegraNet Training Administration"
-admin.site.site_title = "IntegraNet Training"
-admin.site.index_title = "Training Portal Dashboard"
