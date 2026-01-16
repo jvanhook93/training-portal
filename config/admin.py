dcf6c48fe6a7df02d1319dafce00bfb27b293c75
jvanhook93 <106854412+jvanhook93@users.noreply.github.com>
2026-01-16 11:04:05 -0600

    Change title Headings

---
+++ config/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "IntegraNet Training Administration"
+admin.site.site_title = "IntegraNet Training"
+admin.site.index_title = "Training Portal Dashboard"
