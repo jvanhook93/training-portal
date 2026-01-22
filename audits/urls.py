4153f02e8309b1b5a55c0a720dc94c3d3a238c3f
jvanhook93 <106854412+jvanhook93@users.noreply.github.com>
2026-01-21 21:06:36 -0600

    Add Cert Download

--- audits/urls.py
+++ audits/urls.py
@@ -1,9 +1,11 @@
+# audits/urls.py
 from django.urls import path
-from .views import audit_center
 from . import views
 
-
 urlpatterns = [
-    path("", audit_center, name="audit_center"),
-    path("certificates/<str:certificate_id>/download/", views.certificate_download, name="certificate-download"),
+    path(
+        "certificates/<str:certificate_id>/download/",
+        views.certificate_download,
+        name="certificate-download",
+    ),
 ]
