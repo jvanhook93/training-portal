9e10f2fd0cac0bdb394dd3128343744d52527234
jvanhook93 <106854412+jvanhook93@users.noreply.github.com>
2026-02-04 15:13:45 -0600

    Add Branding to Audit Center

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "../public/brand/brand.css";
+import "/brand/brand.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
